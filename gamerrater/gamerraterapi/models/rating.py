9afda77162504102133da960ed56ba07b4dd9319
Alex Martin <alex@subtleco.co>
2021-05-04 15:46:10 -0500

    add rating model

--- gamerrater/gamerraterapi/models/rating.py
+++ gamerrater/gamerraterapi/models/rating.py
@@ -4,3 +4,4 @@ class Rating(models.Model):
     user = models.ForeignKey("User", on_delete=models.CASCADE)
     game = models.ForeignKey("Game", on_delete=models.CASCADE)
     score = models.IntegerField(min=0, max=10)
+    
