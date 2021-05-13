a0208c54daedcce3d165ca31ba65844c0a004093
Alex Martin <alex@subtleco.co>
2021-05-13 12:05:12 -0500

    did the images chapter. I dunno.

--- gamerraterapi/models/image.py
+++ gamerraterapi/models/image.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
-class Image(models.Model):
-    photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+
+class GamePicture(models.Model):
+    game = models.ForeignKey('Game', on_delete=models.DO_NOTHING, related_name='pictures')
+    action_pic = models.ImageField(upload_to='actionimages', height_field=None, width_field=None, max_length=None, null=True)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    timestamp = models.DateTimeField()
+    timestamp = models.DateTimeField(default=timezone.now, editable=False)
